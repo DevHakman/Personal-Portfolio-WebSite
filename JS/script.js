771a57d3cf2b665f23c40eabf944a4c4f30db058
Kage <hamzakman42@gmail.com>
2023-09-27 13:17:22 +0300

    Updated
    Last view of the site with some bugs.

--- JS/script.js
+++ JS/script.js
@@ -29,7 +29,18 @@ window.onscroll = () => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
+
+            //active sections for animation on scroll
+
+            sec.classList.add('show-animate');
         }
+
+        //if want to use animation that repeats on scroll use this >
+        else{
+            sec.classList.remove('show-animate');
+        }
+      
+
     });
 
 
@@ -42,6 +53,11 @@ window.onscroll = () => {
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 
+    //animation footer on scroll
+
+    let footer = document.querySelector('footer');
+
+    footer.classList.toggle('show-animate',this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight );
 
 }
 
